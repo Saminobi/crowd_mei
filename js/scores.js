73e9eb46e9a0b6b823c212ae6107e7c0677a7a48
Saminobi <jpsamiotis@gmail.com>
2019-07-05 17:05:16 +0200

    Working mockup
    - Score is shown to the right
    - Verovio editor is shown to the left

---
+++ js/scores.js
@@ -0,0 +1,5 @@
+function addScore() {
+  var img = document.createElement("score")
+  var source = '/Users/saminobi/Documents/PhD_Work/Projects/Collaborate_MEI/Codebase/collaborate_mei/scores/IMSLP109835-PMLP30951-sibley.1802.1525.beethoven.andante.fmajor.jpg'
+  document.body.appendChild(img)
+}
